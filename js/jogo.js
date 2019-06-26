1e417da4c07de7a5d7ac8dae86b207b3643ca66d
Wanderlei Silva do Carmo <wander.silva@gmail.com>
2019-06-26 15:05:23 -0300

    Criação de variáveis de configuração de tentativas e adição de relógio.

--- js/jogo.js
+++ js/jogo.js
@@ -1,35 +1,51 @@
 var numero;
+var maxtentativas = 7;
+var divmaxtentativas = document.querySelector("#divmaxtentativas");
 var tentativas;
 var tempo;
 var divtentativas = document.querySelector("#divtentativas");
 var resultado = document.querySelector("#resultado");
 var imagem = document.querySelector("#imagem");
+divmaxtentativas.innerHTML = "<strong>" + maxtentativas + "</strong>";
+
 
 function iniciarJogo() {
 
     tentativas = 0;
     tempo = 2000;
+    
     document.querySelector("#palpite").value = "";
-
+    
     numero = parseInt(Math.random() * 100);
     divtentativas.value = tentativas;
     resultado.innerHTML = "";
+    resultado.style.color = "red";
     imagem.src = "img/play.gif";
+
+   
 }
+
+
 
 function adivinha() {
 
     var acertou = false;
     var palpite = parseInt(document.querySelector("#palpite").value);
-
+    
     resultado.innerHTML = "Calculando... aguarde";
-
-    if (tentativas >= 3) {
+    
+    if (tentativas >= maxtentativas) {
 
         alert("Fim de jogo! Número máximo de tentativas, Você perdeu!!! ");
         resultado.style.color = "red";
-        resultado.innerHTML = "O número era..: " + numero;
-        imagem.src = "img/fim.gif";
+        imagem.src = "img/errou.gif";
+
+        setTimeout( function(){
+            resultado.innerHTML = "O número era..: " + numero;
+            imagem.src = "img/fim.gif";
+
+        }, tempo);
+        
         return false;
     }
 
@@ -64,4 +80,18 @@ function adivinha() {
     divtentativas.value = tentativas;
 }
 
+function relogio(){
+
+    var agora = new Date();
+    var hoje = agora.getDate() + "/" + parseInt(agora.getMonth() + 1 ) + "/" + agora.getFullYear();
+    var hora = agora.getHours() + ":" + agora.getMinutes() + ":" + agora.getSeconds();
+
+    document.querySelector("#relogio").innerHTML =  hoje +" "+ hora ;
+}
+
+function mostraRelogio(){
+    setInterval( function(){
+        relogio();
+    },1000);
+}
 
